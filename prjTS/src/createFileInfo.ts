cd949117c2125024eec98048d970edfb582a282e
Guilherme Santiago Silva <santiago@expansiva.com.br>
2024-09-25 10:43:24 -0300

    Update action add readme

--- prjTS/src/createFileInfo.ts
+++ prjTS/src/createFileInfo.ts
@@ -16,6 +16,9 @@ async function getAllFiles(dirPath:string, arrayOfFiles:string[] = []) {
         } else if (stat.isFile() && (filePath.indexOf("\\l") >= 0 || filePath.indexOf("/l") >= 0)) {
             console.log(`file: ${filePath} `);
             arrayOfFiles.push(filePath);
+        } else if (stat.isFile() && ['package.json', 'README.md', 'readme.md', 'tsconfig.json'].includes(file)){
+            console.log(`file: ${filePath} `);
+            arrayOfFiles.push(filePath);
         }
     }
 
