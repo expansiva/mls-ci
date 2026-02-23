806626869722867613e03858e8589ce920b151ea
Guilherme Santiago <57486730+santiagoExpansiva@users.noreply.github.com>
2026-02-23 14:27:49 -0300

    Update runCI.js

--- runCI.js
+++ runCI.js
@@ -1,7 +1,7 @@
 const { runDownload } = require('./download');
 const { runPreCompile } = require('./preCompile');
-//const { runCompileTs, fixFileDefinition, runBuildAll } = require('./compileTs');
-const { runCompileTs, fixFileDefinition } = require('./compileTs_old');
+const { runCompileTs, fixFileDefinition, runBuildAll } = require('./compileTs');
+//const { runCompileTs, fixFileDefinition } = require('./compileTs_old');
 const { runCreateFileInfo } = require('./createFileInfo');
 const { runCompact } = require('./compact');
 const { runCreateTsconfig } = require('./createTsConfig');
@@ -36,13 +36,13 @@ async function runCI() {
         console.log('----------End preCompile------------------');
 
         console.log('----------Start compileTs------------------');
-        //if(COLLAB_PROJECT == '100554'){ await runBuildAll(COLLAB_PROJECT);} else {await runCompileTs();}
-        await runCompileTs();
+        if(COLLAB_PROJECT == '100554'){ await runBuildAll(COLLAB_PROJECT);} else {await runCompileTs();}
+        //await runCompileTs();
         console.log('----------End compileTs------------------');
 
         //console.log('----------Start processCSS------------------');
-        //if(COLLAB_PROJECT != '100554'){ await runProcessCss(COLLAB_PROJECT);}
-        await runProcessCss(COLLAB_PROJECT);
+        if(COLLAB_PROJECT != '100554'){ await runProcessCss(COLLAB_PROJECT);}
+        //await runProcessCss(COLLAB_PROJECT);
         //console.log('----------End processCSS------------------');
 
         console.log('----------Start compileTs Definition------------------');
