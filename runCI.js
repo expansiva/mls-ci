c3a084fc1d1313d105849412f92b2e9e505ad5f4
Guilherme Santiago <57486730+santiagoExpansiva@users.noreply.github.com>
2026-02-23 10:21:16 -0300

    Update runCI.js

--- runCI.js
+++ runCI.js
@@ -1,7 +1,6 @@
 const { runDownload } = require('./download');
 const { runPreCompile } = require('./preCompile');
-//const { runCompileTs, fixFileDefinition, runBuildAll } = require('./compileTs');
-const { runCompileTs, fixFileDefinition } = require('./compileTs_old');
+const { runCompileTs, fixFileDefinition, runBuildAll } = require('./compileTs');
 const { runCreateFileInfo } = require('./createFileInfo');
 const { runCompact } = require('./compact');
 const { runCreateTsconfig } = require('./createTsConfig');
@@ -36,12 +35,12 @@ async function runCI() {
         console.log('----------End preCompile------------------');
 
         console.log('----------Start compileTs------------------');
-        //await runBuildAll(COLLAB_PROJECT);
-        await runCompileTs();
+        await runBuildAll(COLLAB_PROJECT);
+        //await runCompileTs();
         console.log('----------End compileTs------------------');
 
         //console.log('----------Start processCSS------------------');
-        await runProcessCss(COLLAB_PROJECT);
+        //await runProcessCss(COLLAB_PROJECT);
         //console.log('----------End processCSS------------------');
 
         console.log('----------Start compileTs Definition------------------');
