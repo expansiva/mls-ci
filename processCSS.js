8ee1363c0fc9e3b9d39e4e58213485320180a941
Guilherme Santiago <57486730+santiagoExpansiva@users.noreply.github.com>
2026-03-12 10:32:22 -0300

    Update processCSS.js

--- processCSS.js
+++ processCSS.js
@@ -226,8 +226,8 @@ async function addCssWithOutShadowRoot(code, css) {
         const lineToAdd = `if(this.loadStyle) this.loadStyle(\`${css}\`);`
         const lines = code.split('\n');
         const lineMls = Array.from(lines).find((l) => l.trim().startsWith('/// <mls ') && l.trim().endsWith('/>'))
-        const hasEnhancementLit = lineMls ? lineMls.includes('_100554_enhancementLit') : false;
-        if (!hasEnhancementLit || !css) return code;
+        const hasEnhancementBlank = lineMls ? lineMls.includes('_blank') : true;
+        if (hasEnhancementBlank || !css) return code;
 
         let insideClass = false;
         let constructorIndex = -1;
